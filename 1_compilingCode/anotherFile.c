557c28db285282ceeb1533ec01fe60bbb37bf18c
Chris DuPuis <cpdupuis@gmail.com>
2020-08-26 15:43:39 -0700

    Added second file to helloWorld

---
+++ 1_compilingCode/anotherFile.c
@@ -0,0 +1,11 @@
+char* getMessageFromOtherFile(int whichOne) {
+    switch (whichOne) {
+        case 0:
+            return "zero";
+        case 1:
+            return "this is message number one";
+        default:
+            return "this is the default message";
+    }
+ 
+}
--- 1_compilingCode/helloWorld.c
+++ 1_compilingCode/helloWorld.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+char* getMessageFromOtherFile(int);
+
 int main(int argc, char *argv[]) {
     printf("Hello World\n");
+    for (int i=0; i<3; ++i) {
+        printf("Message: %s\n", getMessageFromOtherFile(i));
+    }
     return 0;
 }
