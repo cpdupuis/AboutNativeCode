3c99b2bc06ace4656dd16af5018c388ecc2b01e2
Chris DuPuis <cpdupuis@gmail.com>
2020-08-27 08:15:43 -0700

    Actually use the library version

--- 4_libraryVersioning/runtimeLoader.c
+++ 4_libraryVersioning/runtimeLoader.c
@@ -8,16 +8,18 @@ int main(int argc, char *argv[]) {
     char *(*func)();
     char buf[32];
     if (argc != 2) {
-        fprintf(stderr, "Usage: runtimeLoader version");
+        fprintf(stderr, "Usage: runtimeLoader version\n");
         exit(1);
     }
     snprintf(buf, sizeof(buf), "libsamplelibrary.so.%s", argv[1]);
-    void *lib = dlopen("libsamplelibrary.so", RTLD_NOW);
+    printf("Opening library %s\n", buf);
+    void *lib = dlopen(buf, RTLD_NOW);
     if (!lib) {
         fprintf(stderr, "No library version found: %s\n", buf);
         exit(1);
     }
-    printf("Version: %s\n", getVersion());
+    func = dlsym(lib, "getVersion");
+    printf("Version: %s\n", func());
     for (int i=1; i<=5; ++i) {
         sprintf(buf, "function%d", i);
         func = dlsym(lib, buf);
